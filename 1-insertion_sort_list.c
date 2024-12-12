0bbb6d759047e89b6c26069345ab3822962fa681
pablonudel <pablonudel@gmail.com>
2024-12-12 09:09:50 +0100

    function that sorts an array of integers in ascending order using the Insertion sort

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,40 @@
+#include "sort.h"
+/**
+ * insertion_sort_list - sorts a doubly linked list of integers
+ * in ascending order using the Insertion sort algorithm
+ *
+ * @list: a doubly linked list
+ *
+ * Return: void
+ */
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *current = (*list)->next, *temp;
+
+	if (!list || !*list || !(*list)->next)
+		return;
+
+	while (current)
+	{
+		temp = current;
+		current = current->next;
+
+		while (temp->prev && temp->n < temp->prev->n)
+		{
+			temp->prev->next = temp->next;
+			if (temp->next)
+				temp->next->prev = temp->prev;
+
+			temp->next = temp->prev;
+			temp->prev = temp->prev->prev;
+			temp->next->prev = temp;
+
+			if (temp->prev)
+				temp->prev->next = temp;
+			else
+				*list = temp;
+
+			print_list(*list);
+		}
+	}
+}

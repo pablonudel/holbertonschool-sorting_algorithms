e1d136d9e2acef01d720b3a2f7cdac4cea93fc35
pablonudel <pablonudel@gmail.com>
2024-12-12 10:32:43 +0100

    fixing syntaxis

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,28 +1,33 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "sort.h"
 
 /**
- * main - Entry point
+ * bubble_sort - sorts an array of integers in ascending order
+ * using the Bubble sort algorithm
  *
- * Return: Always 0
+ * @array: array of integers to be sorted
+ * @size: length of the array
+ *
+ * Return: void
  */
 void bubble_sort(int *array, size_t size)
 {
 	size_t i, j, k, temp;
 
+	if (!array || !size)
+		return;
+
 	for (j = 0; j < size; j++)
 	{
 		for (i = 0; i < size; i++)
 		{
-		if (array[i - 1] > array[i])
+			if (array[i - 1] > array[i])
 			{
 				temp = array[i];
 				array[i] = array[i - 1];
 				array[i - 1] = temp;
-				for(k = 0; k < size; k++)
+				for (k = 0; k < size; k++)
 				{
-					printf("%d",array[k]);
+					printf("%d", array[k]);
 					if (k != size - 1)
 						printf(", ");
 
